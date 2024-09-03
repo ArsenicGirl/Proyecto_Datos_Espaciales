c69e6543b41ab929232a5559b1a58b05744d8ba1
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-09-02 23:41:56 -0500

    changes

--- presentacion/index.js
+++ presentacion/index.js
@@ -1,5 +1,6 @@
 // Importar Express
 const express = require('express');
+const path = require('path');
 
 // Inicializar la aplicación Express
 const app = express();
@@ -7,9 +8,12 @@ const app = express();
 // Definir el puerto para el servidor
 const PORT = 3000;
 
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'presentacion')));
+
 // Ruta principal
-app.get('/prueba', (req, res) => {
-    res.send('¡Hola Mundo con Node.js y Express!');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Cambiado 'index.html' a estar dentro de 'presentacion'
 });
 
 // Iniciar el servidor
