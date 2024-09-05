3f60a104c708a97bdfb986a050007c487eb0f23f
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-09-05 14:43:19 -0500

    Logica Front con fetch()

--- Front/logica/app.js
+++ Front/logica/app.js
@@ -1,2 +1,31 @@
 //Para lógica de Front (comunicación con el Back)
 
+document.getElementById('formularioCliente').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const location ={
+        cc: document.getElementById('cc').value,
+        name: document.getElementById('name').value,
+        lastName: document.getElementById('lastName').value,
+        addres: document.getElementById('address').value,
+        latitude: parseFloat(document.getElementById('lat').value),
+        longitude: parseFloat(document.getElementById('lng')),
+    };
+
+    fetch('/api/locations', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(location)
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        console.log('Locación creada:', data);
+        document.getElementById('formularioCliente').reset();
+        alert('Locación creada con éxito');
+    })
+
+    .catch(error => console.error('Error:', error));
+
+
+});
